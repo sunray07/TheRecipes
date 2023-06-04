651bd26334d7b79ec1f3480d008284997e2710fd
Ibragim <91393207+sunray07@users.noreply.github.com>
2023-06-04 20:08:12 +0300

    Add files via upload

---
+++ TheRecipes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheRecipes
+//
+//  Created by Ибрагим Сахратулаев on 04.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
